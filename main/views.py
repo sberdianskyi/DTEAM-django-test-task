2ee07dcb936806d7abca294dad77ab01e66e77c6
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-31 13:52:46 +0300

    added CVDetailView to views.py for detailed CV page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import CV
 
 
@@ -10,3 +10,16 @@ class CVListView(ListView):
     def get_queryset(self):
         queryset = CV.objects.prefetch_related("skills", "projects", "contacts")
         return queryset
+
+
+class CVDetailView(DetailView):
+    model = CV
+    template_name = "main/cv_detail.html"
+    context_object_name = "cv"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["skills"] = self.object.skills.all()
+        context["projects"] = self.object.projects.all()
+        context["contacts"] = self.object.contacts.all()
+        return context
