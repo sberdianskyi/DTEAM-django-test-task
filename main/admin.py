c95522af165117c3c9818ba103222f73c85cab84
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-31 12:32:45 +0300

    registered models in admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import CV, Skill, Project, Contact
 
-# Register your models here.
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 0
+
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 0
+
+
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 0
+
+
+@admin.register(CV)
+class CVAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+    search_fields = ("first_name", "last_name")
+    inlines = [SkillInline, ProjectInline, ContactInline]
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("name", "level", "cv")
+    list_filter = ("level",)
+    search_fields = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "technologies", "start_date", "end_date", "cv")
+    list_filter = ("start_date", "end_date")
+    search_fields = ("name", "technologies")
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("type", "value", "cv")
+    list_filter = ("type",)
+    search_fields = ("value",)
