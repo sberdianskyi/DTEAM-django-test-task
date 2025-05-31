6eb785caf2218e0cb0e288a153672ea8ffaef7f3
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-31 13:52:18 +0300

    added path for CVDetailView in urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CVListView
+from .views import CVListView, CVDetailView
 
 app_name = "main"
 
 urlpatterns = [
     path("", CVListView.as_view(), name="cv_list"),
+    path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
 ]
