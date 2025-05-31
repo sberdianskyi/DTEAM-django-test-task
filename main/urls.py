2227a24497cb83a5acb6f50599e5968954d74100
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-31 22:24:18 +0300

    added routing for CVViewSet to urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
-from .views import CVListView, CVDetailView, CVPDFView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CVListView, CVDetailView, CVPDFView, CVViewSet
 
 app_name = "main"
+
+router = DefaultRouter()
+router.register("api/cvs", CVViewSet)
 
 urlpatterns = [
     path("", CVListView.as_view(), name="cv_list"),
     path("cv/<int:pk>/", CVDetailView.as_view(), name="cv_detail"),
     path("cv/<int:pk>/pdf/", CVPDFView.as_view(), name="cv_pdf"),
+    path("", include(router.urls)),
 ]
