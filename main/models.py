2be0cb481ac2e64994d42ed8fb15c00e51d4575e
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-31 12:31:24 +0300

    added bio field for CV model

--- main/models.py
+++ main/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class CV(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    bio = models.TextField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Skill(models.Model):
-    cv = models.ForeignKey(CV, on_delete=models.CASCADE, related_name='skills')
+    cv = models.ForeignKey(CV, on_delete=models.CASCADE, related_name="skills")
     name = models.CharField(max_length=100)
     level = models.CharField(max_length=50)  # e.g. Beginner, Intermediate, Expert
 
@@ -19,7 +20,7 @@ class Skill(models.Model):
 
 
 class Project(models.Model):
-    cv = models.ForeignKey(CV, on_delete=models.CASCADE, related_name='projects')
+    cv = models.ForeignKey(CV, on_delete=models.CASCADE, related_name="projects")
     name = models.CharField(max_length=255)
     description = models.TextField()
     technologies = models.CharField(max_length=255)
@@ -31,7 +32,7 @@ class Project(models.Model):
 
 
 class Contact(models.Model):
-    cv = models.ForeignKey(CV, on_delete=models.CASCADE, related_name='contacts')
+    cv = models.ForeignKey(CV, on_delete=models.CASCADE, related_name="contacts")
     type = models.CharField(max_length=50)  # e.g. Email, Phone, LinkedIn
     value = models.CharField(max_length=255)
 
